2b106fded10ea92dfb0f9fd53c1fb3fb70a70e0c
TulsiRathod <tulsirathod9102@gmail.com>
2023-05-04 18:06:08 +0530

    q

--- src/baseURL.jsx
+++ src/baseURL.jsx
@@ -1,2 +1,2 @@
-// export const WEB_URL="https://alma-plus-server.vercel.app";
-export const WEB_URL="http://localhost:5000";
+export const WEB_URL="https://alma-plus-server.vercel.app";
+// export const WEB_URL="http://localhost:5000";
